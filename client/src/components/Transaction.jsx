0af5862e437be4b7649a64854f5641c65e2dc020
PC3961 <cpranavss65@gmail.com>
2024-10-18 18:46:06 +0530

    Delete and Add Transactions Working

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/numberWithCommas";
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
@@ -8,9 +9,14 @@ export const Transaction = ({ transaction }) => {
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
-        {sign}${Math.abs(transaction.amount)}
+        {sign}${numberWithCommas(Math.abs(transaction.amount))}
       </span>
-      <button onClick={()=>deleteTransaction(transaction._id)} className="delete-btn">x</button>
+      <button
+        onClick={() => deleteTransaction(transaction._id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
